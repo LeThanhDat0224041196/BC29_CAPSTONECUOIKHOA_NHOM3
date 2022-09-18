1e601eb66b37d402d24ee8587052b8b21f0e8387
LeThanhDat0224041196 <lethanhdat0224@gmail.com>
2022-09-18 15:41:17 +0700

    Upload_4

--- bc29reactjs_duancuoikhoa/src/layouts/Home/Home.jsx
+++ bc29reactjs_duancuoikhoa/src/layouts/Home/Home.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Footer from '../../components/Footer/Footer'
+import Header from '../../components/Header/Header'
 
-export default function Home() {
+export default function HomeLayout() {
   return (
-    <div>Home</div>
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
   )
 }
--- bc29reactjs_duancuoikhoa/src/routes/index.jsx
+++ bc29reactjs_duancuoikhoa/src/routes/index.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { useRoutes } from 'react-router-dom'
+import HomeLayout from '../layouts/Home/Home'
 
 export default function Router() {
-  return (
-    <div>index</div>
-  )
+  const routing = useRoutes([
+    {
+      path: '/',
+      element: <HomeLayout />,
+      children: [
+        {
+          path: '/',
+          element: <Home />
+        }
+      ]
+    }
+  ]);
+  return routing;
 }
