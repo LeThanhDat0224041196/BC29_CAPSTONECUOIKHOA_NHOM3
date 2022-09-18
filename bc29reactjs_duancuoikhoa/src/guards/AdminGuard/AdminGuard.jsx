3ed9ef106256d2d19a4f08e62798864a6ee352ea
LeThanhDat0224041196 <lethanhdat0224@gmail.com>
2022-09-18 14:56:39 +0700

    Upload_3

---
+++ bc29reactjs_duancuoikhoa/src/guards/AdminGuard/AdminGuard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AdminGuard() {
+  return (
+    <div>AdminGuard</div>
+  )
+}
---
+++ bc29reactjs_duancuoikhoa/src/guards/AuthGuard/AuthGuard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AuthGuard() {
+  return (
+    <div>AuthGuard</div>
+  )
+}
