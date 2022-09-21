0717b05b256af2c082ff67307fcededc171c1665
Jasper-Duong <duongngochung13@gmail.com>
2022-09-21 22:07:17 +0700

    Init Header

--- bc29reactjs_duancuoikhoa/src/components/Header/Header.jsx
+++ bc29reactjs_duancuoikhoa/src/components/Header/Header.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import './Header.scss'
+import HeaderList from "./HeaderList/HeaderList";
+import Logo from "./Logo/Logo";
+import SearchHeader from "./SearchHeader/SearchHeader";
+import UserHeader from "./UserHeader/UserHeader";
 export default function Header() {
   return (
     <header>
       <div className="header__wrapper">
-        <div className="max-width-container">
-          
+        <div className="header__bar max-width-container text-white">
+          <Logo/>
+          <SearchHeader/>
+          <HeaderList/>
+          <UserHeader/>
         </div>
       </div>
     </header>
