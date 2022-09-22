2d63dae9e2c36699211f3d6136677e92effbca90
dara <danhutnhi@gmail.com>
2022-09-22 15:28:21 +0700

    update header

--- bc29reactjs_duancuoikhoa/src/components/Header/UserHeader/UserHeader.jsx
+++ bc29reactjs_duancuoikhoa/src/components/Header/UserHeader/UserHeader.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function UserHeader() {
   return (
-    <div>UserHeader</div>
+    <div className='navbar'>
+      <button type="button" class="btn header__btn__sign shadow-none">Sign in</button>
+      <button type="button" class="btn header__btn__join btn-outline-success shadow-none">Join</button>
+    </div>
   )
 }
